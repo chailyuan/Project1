aee1ab4281fc25f3ae1eb055cb5d2c29ce163f27
chailyuan <chailyuan2@gmail.com>
2017-10-26 11:05:33 +0800

    添加项目文件。

---
+++ Project1/lambda.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <functional>
+#include <stdlib.h>
+
+using namespace std;
+
+typedef std::function<bool()> BIGGER;
+
+bool  isbigger(int a, int b,BIGGER big) {
+	return big();
+}
+
+int main() {
+	int a = 4, b = 3;
+
+	auto f = [&] ()mutable->bool {return a > b; };
+
+	bool ret = f();
+
+	if (ret)
+		printf("a>b");
+	else
+		printf("a<b");
+
+	system("pause");
+}
+
